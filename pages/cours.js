86b0228130a9ab7270e5d00382222fea69e75f41
noelakaya <akayanoel@gmail.com>
2025-11-26 14:05:17 +0000

    premier commit

---
+++ pages/cours.js
@@ -0,0 +1,18 @@
+let age = 10
+
+if(age >= 18) {
+        console.log("vous etes majeur !!! ")
+    }
+    else if(age > 50){
+        console.log("vous etes senior !!")
+    }
+    else{
+        console.log("vous etes")
+    }
+    
+let age =
+let handicape = false;
+if (age >=70 // handicape == true){
+    console.log("vous recevrez une prime")
+    
+}
